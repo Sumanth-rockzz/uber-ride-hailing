f7e4b6a1cbe240c921c12fe145d99c7efbb02b54
Sumanth-rockzz <Sumanthn876@gmail.com>
2026-03-20 15:37:46 +0530

    added ride request flow in ride service.

--- services/ride-service/src/repositories/postgresRideRepository.js
+++ services/ride-service/src/repositories/postgresRideRepository.js
@@ -2,16 +2,32 @@ const RideRepository = require('./rideRepository');
 const pool = require('../config/db');
 
 class PostgresRideRepository extends RideRepository {
-  async createRide({ riderId, pickup, destination }) {
-    const result = await pool.query(
-      `INSERT INTO rides (rider_id, pickup, destination, status)
-       VALUES ($1, $2, $3, $4)
-       RETURNING *`,
-      [riderId, pickup, destination, 'REQUESTED']
-    );
+async createRide({ riderId, pickup, destination }) {
+  const result = await pool.query(
+    `INSERT INTO rides (rider_id, pickup, destination, status)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *`,
+    [riderId, pickup, destination, 'REQUESTED']
+  );
 
-    return result.rows[0];
-  }
+  return result.rows[0];
+}
+async getRideById(id) {
+  const result = await pool.query(
+    `SELECT * FROM rides WHERE id = $1`,
+    [id]
+  );
+
+  return result.rows[0];
+}
+async updateRideStatus(id, status) {
+  const result = await pool.query(
+    `UPDATE rides SET status = $1 WHERE id = $2 RETURNING *`,
+    [status, id]
+  );
+
+  return result.rows[0];
+}
 }
 
 module.exports = PostgresRideRepository;
--- services/ride-service/src/repositories/rideRepository.js
+++ services/ride-service/src/repositories/rideRepository.js
@@ -1,5 +1,14 @@
 class RideRepository {
+
   async createRide(data) {
+    throw new Error("Not implemented");
+  }
+
+  async getRideById(id) {
+    throw new Error("Not implemented");
+  }
+
+  async updateRideStatus(id, status) {
     throw new Error("Not implemented");
   }
 }
